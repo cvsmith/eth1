8e2f7f6ec3d055ee87b531984074437283f05de6
swestenberg <swestenb@andrew.cmu.edu>
2015-11-14 11:45:24 -0500

    added comments and reorganized

--- tcp-client.py
+++ tcp-client.py
@@ -1,14 +1,23 @@
 import socket
+import sys
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_PORT = 25000
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
-#my name is sam
+
+
+#Create a TCP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
+
+#Connect our socket to the port where the server is listening at
+server_address = (TCP_IP, TCP_PORT)
+s.connect(server_address)
+
+#Try to send a message
+MESSAGE = "Hello, World!"
+try:
+	s.send(MESSAGE)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
 
 print "received data:", data
