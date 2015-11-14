33e0d6a2a8a4beb98e1d92da559d10cab5b5ff8b
cvsmith <cvsmith@andrew.cmu.edu>
2015-11-13 22:03:45 -0500

    Add comments to sample client

--- tcp-client.py
+++ tcp-client.py
@@ -4,9 +4,16 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
-#my name is sam
+
+# Create TCP socket object s
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Skip optional step to bind s (socket will choose outgoing port for you)
+
+# Connect s to IP/port defined above
 s.connect((TCP_IP, TCP_PORT))
+
+# Treat s like a file now, except with send/recv instead of read/write
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
